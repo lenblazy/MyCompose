fd539aded566f665167be7286241cac086784a22
lenblazy <lenblazy@gmail.com>
2023-09-15 12:28:15 +0300

    Create columnscope extension function

--- app/src/main/java/com/lenibonje/mycompose/MainActivity.kt
+++ app/src/main/java/com/lenibonje/mycompose/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
@@ -13,6 +14,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.lenibonje.mycompose.ui.theme.MyComposeTheme
@@ -34,6 +36,16 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun ColumnScope.CustomItem(weight: Float, color: Color=MaterialTheme.colorScheme.primary){
+    Surface(
+        modifier = Modifier
+            .width(200.dp)
+            .weight(3f),
+        color = MaterialTheme.colorScheme.secondary
+    ) { }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
@@ -42,19 +54,8 @@ fun GreetingPreview() {
             modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Surface(
-                modifier = Modifier
-                    .width(200.dp)
-                    .weight(3f),
-                color = MaterialTheme.colorScheme.secondary
-            ) { }
-            Surface(
-                modifier = Modifier
-                    .width(200.dp)
-                    .weight(2f),
-                color = MaterialTheme.colorScheme.primary
-            ) { }
-
+            CustomItem(weight = 3f)
+            CustomItem(weight = 1f, color =MaterialTheme.colorScheme.secondary)
         }
     }
 }
