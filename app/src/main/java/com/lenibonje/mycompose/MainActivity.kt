bc9d3a4cc2f0583109cad8dced76f02d58b2902b
lenblazy <lenblazy@gmail.com>
2023-09-15 12:22:44 +0300

    Use modifiers to arrange items in column

--- app/src/main/java/com/lenibonje/mycompose/MainActivity.kt
+++ app/src/main/java/com/lenibonje/mycompose/MainActivity.kt
@@ -3,16 +3,19 @@ package com.lenibonje.mycompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.lenibonje.mycompose.ui.theme.MyComposeTheme
-import com.lenibonje.mycompose.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,38 +27,46 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-//                    Greeting("Lennox")
-                    CustomText("Lennox")
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Composable
-fun CustomText(text: String){
-    Text(
-        text = text,
-        style = Typography.bodyMedium
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyComposeTheme {
-        Column {
-            Greeting("Android")
-            CustomText("Lennox")
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
+            Surface(
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(50.dp),
+                color = MaterialTheme.colorScheme.primary
+            ) { }
+            Surface(
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(50.dp),
+                color = MaterialTheme.colorScheme.primary
+            ) { }
+            Surface(
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(50.dp),
+                color = MaterialTheme.colorScheme.primary
+            ) { }
+            Surface(
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(50.dp),
+                color = MaterialTheme.colorScheme.primary
+            ) { }
         }
-
     }
 }
