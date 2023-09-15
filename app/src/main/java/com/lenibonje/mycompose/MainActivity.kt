24019925c7a951c141cca3ccc85ef71c212c53db
lenblazy <lenblazy@gmail.com>
2023-09-15 16:30:18 +0300

    Create Expandable card function

--- app/src/main/java/com/lenibonje/mycompose/MainActivity.kt
+++ app/src/main/java/com/lenibonje/mycompose/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ExpandableCard()
                 }
             }
         }
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyComposeTheme {
-        Greeting("Android")
+        ExpandableCard()
     }
 }
