b0c91b5e369ccb670ce30f626dd1672dfb40eb21
lenblazy <lenblazy@gmail.com>
2023-09-21 10:22:18 +0300

    Remove parameters from greeting button

--- app/src/main/java/com/lenibonje/mycompose/MainActivity.kt
+++ app/src/main/java/com/lenibonje/mycompose/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -30,17 +30,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting() {
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyComposeTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
