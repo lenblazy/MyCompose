77234a1c4ae8679cb03c0cfa95839319c5195b2b
lenblazy <lenblazy@gmail.com>
2023-10-08 11:55:07 +0300

    Add sticky headers to lazy column

--- app/src/main/java/com/lenibonje/mycompose/MainActivity.kt
+++ app/src/main/java/com/lenibonje/mycompose/MainActivity.kt
@@ -3,9 +3,13 @@ package com.lenibonje.mycompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
@@ -13,6 +17,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.lenibonje.mycompose.repository.PersonRepository
@@ -30,15 +35,31 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun Greeting() {
-    val personRepository = PersonRepository()
-    val persons = personRepository.getAllData()
 
-    LazyColumn(contentPadding = PaddingValues(all = 12.dp),
-        verticalArrangement = Arrangement.spacedBy(12.dp)) {
-        items(items = persons) { person ->
-            CustomItem(person = person)
+    val sections = listOf<String>("a","b","c","d","e","f","g","h","i")
+
+    LazyColumn(
+        contentPadding = PaddingValues(all = 12.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        sections.forEach { section ->
+            stickyHeader {
+                Text(
+                    text = "Section $section",
+                    modifier = Modifier.fillMaxWidth()
+                        .background(Color.LightGray)
+                        .padding(12.dp)
+                )
+            }
+            items(10) {
+                Text(text = "Item $it in section $section",
+                    modifier = Modifier
+                        .padding(12.dp)
+                )
+            }
         }
     }
 }
