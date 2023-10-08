f4b025444ca9fb3e215428297897a6c4f2ac7b6a
lenblazy <lenblazy@gmail.com>
2023-10-08 14:32:01 +0300

    Custom component fully functionable

--- app/src/main/java/com/lenibonje/mycompose/CustomComponent.kt
+++ app/src/main/java/com/lenibonje/mycompose/CustomComponent.kt
@@ -1,12 +1,16 @@
 package com.lenibonje.mycompose
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.animate
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateIntAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -14,6 +18,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.AbsoluteAlignment.TopLeft
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
@@ -23,8 +28,11 @@ import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import kotlin.math.min
 
@@ -35,48 +43,77 @@ fun CustomComponent(
     maxIndicatorValue: Int = 100,
     backgroundIndicatorColor: Color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f),
     backgroundIndicatorStrokeWidth: Float = 100f,
+    indicatorStrokeCap: StrokeCap = StrokeCap.Round,
     foregroundIndicatorColor: Color = MaterialTheme.colorScheme.primary,
     foregroundIndicatorStrokeWidth: Float = 100f,
+    bigTextFontSize: TextUnit = MaterialTheme.typography.titleLarge.fontSize,
+    bigTextColor: Color = MaterialTheme.colorScheme.onSurface,
+    bigTextSuffix: String = "GB",
+    smallText: String = "Remaining",
+    smallTextFontSize: TextUnit = MaterialTheme.typography.bodySmall.fontSize,
+    smallTextColor: Color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.3f),
 ) {
 
     val allowedIndicatorValue by remember {
         mutableStateOf(min(indicatorValue, maxIndicatorValue))
     }
 
-    val animatedIndicatorValue = remember {
-        Animatable(initialValue = 0f)
-    }
-
+    var animatedIndicatorValue by remember { mutableStateOf(0f) }
     LaunchedEffect(key1 = allowedIndicatorValue) {
-        animatedIndicatorValue.animateTo(
-            targetValue = allowedIndicatorValue.toFloat()
-        )
+        animatedIndicatorValue = allowedIndicatorValue.toFloat()
     }
 
-    val percentage = (animatedIndicatorValue.value / maxIndicatorValue) * 100
+    val percentage = (animatedIndicatorValue / maxIndicatorValue) * 100
 
     val sweepAngle by animateFloatAsState(
         targetValue = (percentage * 2.4).toFloat(),
-        animationSpec = tween(1000)
+        animationSpec = tween(1000), label = ""
     )
 
-    Column(modifier = Modifier
-        .size(canvasSize)
-        .drawBehind {
-            val componentSize = size / 1.25f
-            backgroundIndicator(
-                componentSize = componentSize,
-                indicatorColor = backgroundIndicatorColor,
-                indicatorStrokeWidth = backgroundIndicatorStrokeWidth
-            )
-            foregroundIndicator(
-                sweepAngle = sweepAngle,
-                componentSize = componentSize,
-                indicatorColor = foregroundIndicatorColor,
-                indicatorStrokeWidth = foregroundIndicatorStrokeWidth
-            )
-        }
-    ) {}
+    val receivedValue by animateIntAsState(
+        targetValue = allowedIndicatorValue,
+        animationSpec = tween(1000),
+        label = ""
+    )
+
+    val animatedBigTextColor by animateColorAsState(
+        targetValue = if (allowedIndicatorValue == 0)
+            MaterialTheme.colorScheme.onSurface.copy(alpha = 0.3f) else bigTextColor,
+        animationSpec = tween(1000),
+        label = "",
+    )
+
+    Column(
+        modifier = Modifier
+            .size(canvasSize)
+            .drawBehind {
+                val componentSize = size / 1.25f
+                backgroundIndicator(
+                    componentSize = componentSize,
+                    indicatorColor = backgroundIndicatorColor,
+                    indicatorStrokeWidth = backgroundIndicatorStrokeWidth
+                )
+                foregroundIndicator(
+                    sweepAngle = sweepAngle,
+                    componentSize = componentSize,
+                    indicatorColor = foregroundIndicatorColor,
+                    indicatorStrokeWidth = foregroundIndicatorStrokeWidth,
+                    indicatorStrokeCap = indicatorStrokeCap
+                )
+            },
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        EmbeddedElements(
+            bigText = receivedValue,
+            bigTextFontSize = bigTextFontSize,
+            bigTextColor = animatedBigTextColor,
+            bigTextSuffix = bigTextSuffix,
+            smallText = smallText,
+            smallTextFontSize = smallTextFontSize,
+            smallTextColor = smallTextColor
+        )
+    }
 }
 
 fun DrawScope.backgroundIndicator(
@@ -107,6 +144,7 @@ fun DrawScope.foregroundIndicator(
     componentSize: Size,
     indicatorColor: Color,
     indicatorStrokeWidth: Float,
+    indicatorStrokeCap: StrokeCap,
 ) {
     drawArc(
         size = componentSize,
@@ -116,13 +154,38 @@ fun DrawScope.foregroundIndicator(
         useCenter = false,
         style = Stroke(
             width = indicatorStrokeWidth,
-            cap = StrokeCap.Round,
+            cap = indicatorStrokeCap,
             join = StrokeJoin.Round
         ),
         topLeft = Offset(
             x = (size.width - componentSize.width) / 2f,
             y = (size.height - componentSize.height) / 2f
         ),
+    )
+}
+
+@Composable
+fun EmbeddedElements(
+    bigText: Int,
+    bigTextFontSize: TextUnit,
+    bigTextColor: Color,
+    bigTextSuffix: String,
+    smallText: String,
+    smallTextFontSize: TextUnit,
+    smallTextColor: Color,
+) {
+    Text(
+        text = smallText,
+        fontSize = smallTextFontSize,
+        color = smallTextColor,
+        textAlign = TextAlign.Center
+    )
+    Text(
+        text = "$bigText ${bigTextSuffix.take(2)}",
+        fontSize = bigTextFontSize,
+        color = bigTextColor,
+        textAlign = TextAlign.Center,
+        fontWeight = FontWeight.Bold
     )
 }
 
