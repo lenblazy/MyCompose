5177f6d340fcf772dc3ed36dfc614e4faef756f8
lenblazy <lenblazy@gmail.com>
2023-10-08 11:49:18 +0300

    Add spaces to lazy column

--- app/src/main/java/com/lenibonje/mycompose/MainActivity.kt
+++ app/src/main/java/com/lenibonje/mycompose/MainActivity.kt
@@ -3,13 +3,19 @@ package com.lenibonje.mycompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.lenibonje.mycompose.repository.PersonRepository
 import com.lenibonje.mycompose.ui.theme.MyComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,12 +24,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting()
-                }
+                Greeting()
             }
         }
     }
@@ -31,7 +32,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting() {
+    val personRepository = PersonRepository()
+    val persons = personRepository.getAllData()
 
+    LazyColumn(contentPadding = PaddingValues(all = 12.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)) {
+        items(items = persons) { person ->
+            CustomItem(person = person)
+        }
+    }
 }
 
 @Preview(showBackground = true)
