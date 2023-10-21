d5a9ecc00ad387eb54f1cfd4b5310524fc90ee25
lenblazy <lenblazy@gmail.com>
2023-10-21 08:19:50 +0300

    Add swipeable component

--- app/src/main/java/com/lenibonje/mycompose/MainActivity.kt
+++ app/src/main/java/com/lenibonje/mycompose/MainActivity.kt
@@ -1,26 +1,63 @@
 package com.lenibonje.mycompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.twotone.Email
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
-import com.lenibonje.mycompose.navigation.SetUpNavGraph
+import androidx.compose.ui.unit.dp
 import com.lenibonje.mycompose.ui.theme.MyComposeTheme
+import me.saket.swipe.SwipeAction
+import me.saket.swipe.SwipeableActionsBox
 
 class MainActivity : ComponentActivity() {
 
-    lateinit var navController: NavHostController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyComposeTheme {
                 // A surface container using the 'background' color from the theme
-                navController = rememberNavController()
-                SetUpNavGraph(navController = navController)
-//                MainScreen()
+                val archive = SwipeAction(
+                    icon = rememberVectorPainter(Icons.TwoTone.Email),
+                    background = Color.Green,
+                    onSwipe = { Log.d("Swipe", "Archive") }
+                )
+
+                val snooze = SwipeAction(
+                    icon = { Text("Snooze") },
+                    background = Color.Yellow,
+                    isUndo = true,
+                    onSwipe = { Log.d("Swipe", "Snooze") },
+                )
+
+                SwipeableActionsBox(
+                    startActions = listOf(archive),
+                    endActions = listOf(snooze)
+                ) {
+                    Greeting()
+                }
             }
         }
     }
@@ -28,7 +65,38 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.surface)
+            .padding(horizontal = 16.dp)
+            .padding(vertical = 8.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .clip(CircleShape)
+                .background(MaterialTheme.colorScheme.primary)
+                .size(50.dp)
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Column(modifier = Modifier.weight(7f)) {
+            Text(
+                text = "Title",
+                style = TextStyle(
+                    fontSize = MaterialTheme.typography.headlineMedium.fontSize,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            )
+            Text(
+                text = "Some random text",
+                style = TextStyle(
+                    fontSize = MaterialTheme.typography.bodySmall.fontSize,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            )
+        }
 
+    }
 }
 
 @Preview(showBackground = true)
